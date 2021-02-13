c4a6bc725aff8969156742ddedb61d48d6780310
Nami Mac <namimac@Namis-Mac-mini.local>
2021-02-13 07:59:13 +0545

    Initial Commit

---
+++ DemoFeb13/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoFeb13
+//
+//  Created by Nami Mac on 2/13/21.
+//  Copyright © 2021 NAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
